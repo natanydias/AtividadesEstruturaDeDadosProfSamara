bb2df8598d27e7886a7481f31257e334ae95d9ea
natanydias <natanydias@gmail.com>
2025-08-20 13:29:29 -0300

    Primeiro commit - Adicionando código Java

---
+++ Main.java
@@ -0,0 +1,10 @@
+// Acrescente seu nome na posição 3 do vetor e exiba-o em tela
+public class Main {
+    public static void main(String[] args) {
+        String nomes[] = new String[5];
+        nomes[0] = "Samara";
+        nomes[3] = "Natany";
+
+        System.out.println(nomes[3]);
+    }
+}
