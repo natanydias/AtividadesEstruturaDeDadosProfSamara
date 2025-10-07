6e0b0606d63a265d03c459c55b95ae473b63867d
Natâny Dias <natanydias@gmail.com>
2025-10-07 20:39:40 -0300

    Atividades Slide Aula 05 - de Árvores

---
+++ Exerciciosaula05arvores.java
@@ -0,0 +1,11 @@
+//01 - QUAL O 3º ELEMENTO DO PERCURSO EM-ORDEM?
+// RESPOSTA: (5) Ordem correta 2, 7, 5, 6, 11, 2, 5, 4, 9
+
+//02 - 02 - QUAL O 5º ELEMENTO DO PERCURSO PÓS-ORDEM?
+// RESPOSTA: (7) Ordem correta 2, 5, 11, 6, 7, 4, 9, 5, 2
+
+//03 - QUAL O 1º ELEMENTO DO PERCURSO PRE-ORDEM?
+// RESPOSTA:(2) Ordem correta 2, 7, 2, 6, 5, 11, 5, 9, 4
+
+//04 - QUAL A SOMA DO 6º E DO 7º ELEMENTO DO PERCURSO PRÉ-ORDEM?
+// RESPOSTA: (16) Ordem correta 2, 7, 2, 6, 5, 11, 5, 9, 4 (11+5= 16)
